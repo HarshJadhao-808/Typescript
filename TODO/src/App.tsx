b1c39da9ba3b6d196af99d1936581c7e0a099b5d
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2025-12-06 15:14:24 +0530

    working

---
+++ TODO/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  let day : String = "Saturday"
+
+  return (
+		<>
+			<h1 className='text-6xl font-bold text-[green] sm:text-[red] text-center my-100'>{day}</h1>
+		</>
+	);
+}
+
+export default App
