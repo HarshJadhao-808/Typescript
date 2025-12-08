fa628c524f19c7fe19b963af73449aa976fb30ff
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2025-12-08 17:29:53 +0530

    interface created

--- TODO/src/components/Input.tsx
+++ TODO/src/components/Input.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Input = () => {
+	
+	interface Todo {
+		task:string,
+		priority:string
+	}
+	
+	const [todo,setTodo] = useState<Todo>({
+		task:"",
+		priority:""
+	})
+	const sendIt = () => {
+		event?.preventDefault()
+		console.log(todo)
+	}
+
+	const Changes = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+		setTodo({...todo,[e.target.name]:e.target.value})
+		
+	}
+
   return (
-		<div className="h-100 w-[50%]  border- p-8 grid items-center  ">
+		<div className="h-100 w-[50%]  border-  grid items-center  ">
 			<h1 className="text-center font-medium text-3xl sm:text-4xl border-  ">Add TODO</h1>
-			<form className="flex justify-center flex-col border-2 w-full h-full gap-5 ">
+			<form onSubmit={sendIt} className="flex justify-center flex-col border-4 w-80 sm:w-120  h-full gap-3 sm:gap-5 ">
 				<span className="grid ">
 					<label className="text-center mb-3 font-medium text-2xl">Task</label>
-					<input className="border-2 h-8 w-60 m-auto  text-center " type="text" />
+					<input onChange={Changes} name='task' className="border-2 h-8 w-60 m-auto  text-center " type="text" />
 				</span>
 				<span className="grid ">
 					<label className="text-center mb-3 font-medium text-2xl">Priority</label>
-					<select className="border-2 h-8 w-60 m-auto ">
+					<select onChange={Changes} name='priority' className="border-2 h-8 w-60 m-auto ">
 						<option className="text-center" value="none">
 							None
 						</option>
--- TODO/src/components/Table.tsx
+++ TODO/src/components/Table.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const Table = () => {
+  
   return (
-    <div className='border-2 w-[50%]'>
-      
+    <div className='border-4 w-[90%] h-130 overflow-hidden'>
+      <table className='w-full h-full border-4'>
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Priority</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </table>
     </div>
   )
 }
