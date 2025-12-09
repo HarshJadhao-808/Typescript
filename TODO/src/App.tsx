bf99edc53745e31bb87a584064f32cbdff8e96ee
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2025-12-09 17:34:11 +0530

    some work

--- TODO/src/App.tsx
+++ TODO/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 			<h1 className="text-[#000000] font-bold text-center text-3xl sm:text-5xl my-5">
 				TODO APP
 			</h1>
-			<div className="border- grid lg:grid-cols-2 w-[96%] m-auto h-150 justify-center px-25 items-center shadow-[0_4px_20px_rgba(0,0,0,0.25)]">
+			<div className="border- grid lg:grid-cols-2 w-[96%] m-auto h-250 lg:h-150 justify-center px-25 items-center shadow-[0_4px_20px_rgba(0,0,0,0.25)]">
 				<Input />
 				<Table/>
 			</div>
--- TODO/src/components/Table.tsx
+++ TODO/src/components/Table.tsx
@@ -3,17 +3,16 @@ import React from 'react'
 const Table = () => {
   
   return (
-    <div className='border-4 w-[90%] h-130 overflow-hidden'>
-      <table className='w-full h-full border-4'>
-        <thead>
+    <div className='border-4 w-[100%] h-130 overflow-y-auto'>
+      <table className='w-full h-full  '>
+        <thead className='sticky top-0 bg-white ' >
           <tr>
             <th>Task</th>
             <th>Priority</th>
             <th>Delete</th>
           </tr>
         </thead>
-        <tbody>
-          
+        <tbody>  
         </tbody>
       </table>
     </div>
